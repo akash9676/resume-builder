3cbab9818a29fc06ad1519fc747e39c756465321
SaiThejaKunta <saitheja.kunta@gmail.com>
2023-07-03 04:10:27 +0530

    smalltrial

--- src/components/personalDetails.jsx
+++ src/components/personalDetails.jsx
@@ -1,16 +1,61 @@
-import { TextField } from '@mui/material';
+import { TextField } from "@mui/material";
 export const PersonalDetails = () => {
-  return(
+  return (
     <>
       <div>
         <div>
           <h1 className="text-2xl font-bold">Personal Details</h1>
         </div>
         <div>
-          <div className="text-sm font-light">
-            Wanted Job Title 
+          <div>
+            <div className="text-sm font-light">Wanted Job Title</div>
+            <TextField
+              id="filled-helperText"
+              label=""
+              defaultValue="Ex:GDSC Lead"
+              variant="filled"
+            />
           </div>
-          <TextField id="standard-basic" label="Eg:GDSC Lead" variant="standard" />
+        </div>
+
+        <div>
+          <div className="w-screen">
+            <TextField id="First Name" label="First Name" variant="filled" />
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+            <TextField id="Last Name" label="Last Name" variant="filled" />
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+            <TextField id="Email" label="Email" variant="filled" />
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+            <TextField id="Phone" label="Phone" variant="filled" />
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+            <TextField id="Country" label="Country" variant="filled" />
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <div></div>
+          </div>
         </div>
       </div>
     </>

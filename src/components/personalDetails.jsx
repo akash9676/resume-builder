15af24f81ca1dd85886af98db5cb925362a4a80e
SaiThejaKunta <saitheja.kunta@gmail.com>
2023-07-03 03:23:45 +0530

    personalde

--- src/components/personalDetails.jsx
+++ src/components/personalDetails.jsx
@@ -1,9 +1,16 @@
+import { TextField } from '@mui/material';
 export const PersonalDetails = () => {
   return(
     <>
       <div>
         <div>
           <h1 className="text-2xl font-bold">Personal Details</h1>
+        </div>
+        <div>
+          <div className="text-sm font-light">
+            Wanted Job Title 
+          </div>
+          <TextField id="standard-basic" label="Eg:GDSC Lead" variant="standard" />
         </div>
       </div>
     </>

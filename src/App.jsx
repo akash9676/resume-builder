66fe72b04fb50486b82c9dba2ba741bf8af36724
akash9676 <akashnaidu545@gmail.com>
2023-07-03 00:53:20 +0530

    spacing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold">Final testing</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className="text-3xl font-bold">Final testing</h1>
+        </>
+    );
 }
 
 export default App;

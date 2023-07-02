51d3bdb7c66417bea33de70b969603f60d9549aa
akash9676 <akashnaidu545@gmail.com>
2023-07-03 04:05:38 +0530

    shifting to akash branch

--- src/components/personalDetails.jsx
+++ src/components/personalDetails.jsx
@@ -1,11 +1,11 @@
 export const PersonalDetails = () => {
-  return(
-    <>
-      <div>
-        <div>
-          <h1 className="text-2xl font-bold">Personal Details</h1>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <div>
+                    <h1 className="text-2xl font-bold">Personal Details</h1>
+                </div>
+            </div>
+        </>
+    );
 };
--- src/components/reactpdf.jsx
+++ src/components/reactpdf.jsx
@@ -6,20 +6,19 @@ import {
     StyleSheet,
     Line,
     Svg,
+    PDFViewer,
 } from "@react-pdf/renderer";
 
 // Create styles
 const styles = StyleSheet.create({
-    page: {
-        flexDirection: "row",
-    },
+    page: {},
     name: {
-        marginTop: 20,
-        gap: 10,
+        margin: 20,
         display: "flex",
-        flexGrow: 1,
-        fontSize: 25,
+        flexDirection: "row",
         justifyContent: "center",
+        fontWeight: "thin",
+        fontSize: 20,
     },
     links: {
         marginLeft: 20,
@@ -27,6 +26,7 @@ const styles = StyleSheet.create({
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-between",
+        fontSize: 14,
     },
     linkleft: {
         display: "flex",
@@ -40,43 +40,57 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "flex-end",
     },
+    field: {
+        marginTop: 10,
+        marginBottom: 5,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        fontSize: 15,
+    },
+    line: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+    },
 });
 
 // Create Document Component
 const Reactpdf = () => (
-    <Document>
-        <Page size="A4" style={styles.page}>
-            <View style={styles.name}>
-                <Text>Gundapuneni</Text>
-                <Text>Akash</Text>
-            </View>
-            <View style={styles.links}>
-                <View style={styles.linkleft}>
-                    <Text>LinkedIn</Text>
-                    <Text>GitHub</Text>
+    <PDFViewer className="h-screen w-screen">
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.name}>
+                    <Text>Gundapuneni Akash</Text>
                 </View>
-                <View style={styles.linkright}>
-                    <Text>akashnaidu545@gmail.com</Text>
-                    <Text>Portfolio</Text>
+                <View style={styles.links}>
+                    <View style={styles.linkleft}>
+                        <Text>LinkedIn</Text>
+                        <Text>GitHub</Text>
+                    </View>
+                    <View style={styles.linkright}>
+                        <Text>akashnaidu545@gmail.com</Text>
+                        <Text>Portfolio</Text>
+                    </View>
                 </View>
-            </View>
-            <View>
-                <Text>Education</Text>
-            </View>
-            <View>
-                <Svg height="20" width="1000">
-                    <Line
-                        x1="0"
-                        y1="0"
-                        x2="600"
-                        y2="0"
-                        strokeWidth={3}
-                        stroke="rgb(0,0,0)"
-                    />
-                </Svg>
-            </View> 
-        </Page>
-    </Document>
+                <View style={styles.field}>
+                    <Text>Education</Text>
+                </View>
+                <View style={styles.line}>
+                    <Svg height="20" width="550">
+                        <Line
+                            x1="0"
+                            y1="0"
+                            x2="600"
+                            y2="0"
+                            strokeWidth={3}
+                            stroke="rgb(0,0,0)"
+                        />
+                    </Svg>
+                </View>
+            </Page>
+        </Document>
+    </PDFViewer>
 );
 
 export default Reactpdf;
